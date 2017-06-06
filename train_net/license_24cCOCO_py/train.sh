8722f5d5ec7e153abe72af50f13063b69d9fb980
qinhuan <xiaoyu1qh1@163.com>
2017-06-06 15:26:21 +0800

    init

--- train_net/license_24cCOCO_py/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# setting caffe root folder
-CAFFE_ROOT=/home/work/qinhuan/mywork/caffe
-CURRENT_DIR=`pwd`
-# add pycaffe path
-export PYTHONPATH=$PYTHONPATH:$CAFFE_ROOT/python:$CURRENT_DIR
-
-nohup python ./run.py >log.txt 2>&1 &
-#python ./run.py
